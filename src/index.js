4396a8f6ad5deb4ccae277d104531d962ab00199
Manish Kumar Pandey <896849@cognizant.com>
2024-02-10 20:15:27 +0530

    updated the card project

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <>
+      <img className="avatar" src="jonas.jpeg" alt="Jonas Schmedtmann" />
+    </>
+  );
+}
+
+function Intro() {
+  return (
+    <>
+      <h1>Jonas Schmedtmann</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" color="blue" />
+      <Skill skill="HTML+CSS" color="orange" />
+      <Skill skill="JavaScript" color="yellow" />
+      <Skill skill="Svelte" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
